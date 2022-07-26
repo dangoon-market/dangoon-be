7326925c9accc8cdf7f86ce9c0a05046ddf5a447
전찬의 <jcu011@naver.com>
2022-07-27 00:09:02 +0900

    fix : 빌드 후 조치 스크립트 오류 해결 테스트 push

--- src/main/java/dangoonmarket/HashController.java
+++ src/main/java/dangoonmarket/HashController.java
@@ -20,7 +20,7 @@ public class HashController {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "Updated java lsof hello";
+        return "fixed hello";
     }
 
     private String getMD5Digest(String input) throws NoSuchAlgorithmException {
